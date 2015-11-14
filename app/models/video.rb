c1f21a9c52235d97f6dfdfe20b5401293d17bf15
Christopher Cano <canoc4262@gmail.com>
2015-11-14 04:00:12 -0800

    Finish search_by_title for Video model method and tests.

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,9 @@ class Video < ActiveRecord::Base
 	validates :title, presence: true
 	validates :description, presence: true
 	#validates_presence_of :title, :description, use this for many presence validations
+
+	def self.search_by_title(title)
+		return [] if title.blank?
+		title_search_arr = self.where("title LIKE ?", "%#{title}%").order("created_at DESC")
+	end
 end
