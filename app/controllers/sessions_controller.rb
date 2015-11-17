a55258d7b51ea8bc8a76b8585651eb51e07b6d0d
Christopher Cano <canoc4262@gmail.com>
2015-11-16 21:26:34 -0800

    Add front route and view.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+
+  def front
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  root to: "videos#index"
+  #root to: "videos#index"
+  root to: "sessions#front"
 
   get '/home', to: "videos#index"
   #get '/videos/:id',  to: "videos#show", as: "video" #delete this later and use resources?
