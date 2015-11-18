74a4ffb5bfcd07a5fe53fff040f49fecb5a065d8
Christopher Cano <canoc4262@gmail.com>
2015-11-17 20:57:25 -0800

    Finish register and sign in forms/paths, and authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  #root to: "videos#index"
-  root to: "sessions#front"
+  root to: "pages#front"
 
   get '/home', to: "videos#index"
+  get '/register', to: "users#new"#, as: "register"
+  resources :users, only: [:create]
+  get '/sign_in', to: "sessions#new"
+  resources :sessions, only: [:create]
+  #post '/sign_in', to: "sessions#create"
+  get '/sign_out', to: "sessions#destroy"
   #get '/videos/:id',  to: "videos#show", as: "video" #delete this later and use resources?
   resources :videos, only: [:show] do
   	collection do
